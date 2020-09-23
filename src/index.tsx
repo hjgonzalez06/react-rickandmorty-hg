17f697158fda45b2500b82fedfcee5778d7804ff
Hiram J. González <hiramjgonzalez98@gmail.com>
2020-09-23 01:39:06 -0400

    Set up index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './app/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Store
+import generateStore from './app/redux/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let store = generateStore();
+
+const WithRouter = () => <BrowserRouter><App /></BrowserRouter>;
+const WithStore = () => <Provider store={store}><WithRouter /></Provider>;
+
+
+ReactDOM.render(<WithStore />,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
