ee454a4ffb521b78537c201cceed0d0fd12ce774
Hiram J. González <hiramjgonzalez98@gmail.com>
2020-10-05 02:20:23 -0400

    Ready with Character's detailed modal

--- src/app/components/cards/detailed/CharacterModal.tsx
+++ src/app/components/cards/detailed/CharacterModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Modal, Container, Row, Col, Image, ListGroup, Button } from 'react-bootstrap';
 import { DetailedCharacter } from '../../../../types';
+import Style from './modals.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,37 +12,39 @@ function CharacterModal({ character, show, setShow }: DetailedCharacter){
         <>
 
             <Modal show={show} onHide={() => setShow(false)} backdrop="static" keyboard={false} centered>
-                <Modal.Header>
-                    <Modal.Title>{character?.name}</Modal.Title>
+                <Modal.Header className={Style.bgHeader}>
+                    <Modal.Title className="mx-auto font-weight-bolder">{character.name}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body className="show-grid">
                     <Container>
                         <Row>
-                            <Col xs={12} md={12} lg={6} xl={6}>
-                                <Image src={character?.image} fluid />
+                            <Col xs={12} md={12} lg={6} xl={6} className="d-flex justify-content-center">
+                                <Image src={character.image} fluid />
                             </Col>
-                            <Col xs={12} md={12} lg={6} xl={6}>
+                            <Col xs={12} md={12} lg={6} xl={6} className="d-flex flex-column justify-content-center">
                                 <ListGroup>
-                                    <ListGroup.Item>
+                                    <ListGroup.Item className={`${Style.bgListItem} mt-3 mt-lg-3`}>
                                         Type:
                                     </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        {character.type}
+                                    <ListGroup.Item className={`${Style.bgListItem} text-center`}>
+                                        {character.type === "" ?
+                                            <span className="text-muted">Oops, there's no info</span>
+                                            : character.type}
                                     </ListGroup.Item>
                                 </ListGroup>
                                 <ListGroup className="mt-3">
-                                    <ListGroup.Item>
+                                    <ListGroup.Item className={Style.bgListItem}>
                                         Gender:
                                     </ListGroup.Item>
-                                    <ListGroup.Item>
+                                    <ListGroup.Item className={`${Style.bgListItem} text-center`}>
                                         {character.gender}
                                     </ListGroup.Item>
                                 </ListGroup>
                                 <ListGroup className="mt-3">
-                                    <ListGroup.Item>
+                                    <ListGroup.Item className={Style.bgListItem}>
                                         Species:
                                     </ListGroup.Item>
-                                    <ListGroup.Item>
+                                    <ListGroup.Item className={`${Style.bgListItem} text-center`}>
                                         {character.species}
                                     </ListGroup.Item>
                                 </ListGroup>
@@ -49,8 +52,9 @@ function CharacterModal({ character, show, setShow }: DetailedCharacter){
                         </Row>
                     </Container>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={() => setShow(false)}>
+                <Modal.Footer className="d-flex justify-content-center">
+                    <Button onClick={() => setShow(false)} className={`${Style.closeButton} w-25
+                                                                       d-flex justify-content-center`}>
                         <FontAwesomeIcon icon={faTimes} />
                     </Button>
                 </Modal.Footer>
